10fd46dd2438ca0f017c4cc9df63bc36c178ac22
Oskar Grzelak <oskar.grzelak@gmail.com>
2021-10-13 15:15:09 +0200

    fix authentication - the jwt token is not saved correctly in cookies - unnecessary routes have been deleted - logged user data can be gotten using the jwt token in cookies

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util')
 const jwt = require('jsonwebtoken')
 const User = require('./../models/userModel')
 
@@ -18,6 +19,13 @@ exports.signup = async (req, res, next) => {
 
     const token = signToken(newUser._id)
 
+    res.cookie('jwt', token, {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+    })
+
     res.status(201).json({
       status: 'success',
       token,
@@ -36,6 +44,7 @@ exports.signup = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body
+    console.log("LOGIN", password)
 
     if (!email || !password) {
       throw new Error('Please provide email and password')
@@ -49,11 +58,76 @@ exports.login = async (req, res, next) => {
     }
 
     token = signToken(user._id)
+    console.log(token)
+
+    res.cookie('jwt', token, {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+    })
 
     res.status(200).json({
       status: 'success',
       token,
+      user
     })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    })
+  }
+}
+
+exports.loggedUser = async (req, res, next) => {
+  console.log('getting logged user')
+  try {
+    let user = null
+    let token
+    if (req.headers.cookie && req.headers.cookie.startsWith('jwt')) {
+      token = req.headers.cookie.split('=')[1]
+      const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+      user = await User.findById(decoded.id)
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    })
+  }
+}
+
+exports.protect = async (req, res, next) => {
+  try {
+    let token
+    if (
+      req.headers.cookie &&
+      req.headers.cookie.startsWith('jwt')
+    ) {
+      token = req.headers.cookie.split('=')[1]
+    }
+    if (!token) {
+      throw new Error('You are not logged in')
+    }
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+    const freshUser = await User.findById(decoded.id)
+    if (!freshUser) {
+      throw new Error('The user belonging to the token no longer exists')
+    }
+
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+      throw new Error('User recently changed password. Please log in again')
+    }
+    req.user = freshUser
+    next()
   } catch (error) {
     res.status(404).json({
       status: 'fail',
